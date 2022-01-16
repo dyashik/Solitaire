6f53dbe84fad4a492dc0918de6ca39157ff71428
Yashik <30672986+dyashik@users.noreply.github.com>
2022-01-16 11:57:49 -0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,21 +1,3 @@
-/**
- * Author: Yashik Dhanaraj
- * Period: 5
- * Date: 12/2
- * Time Taken: 2hr
- *   
- * Summary: I did much better than I thought. I had an error but it
- * didn't turn out to be my understanding but it was some glitch with the 
- * compiler stuff. But Mr. McLeod was able to fix it. Something really neat
- * that i did was I had a printList method which printed the list with the 
- * suits next to the symbol to make it a more user friendly.
- *  
- **/  
-/** 
- * Card.java
- *
- * <code>Card</code> represents a basic playing card.
- */
 public class Card implements Comparable<Card> {
     /** String value that holds the symbol of the card.
     Examples: "A", "Ace", "10", "Ten", "Wild", "Pikachu"
