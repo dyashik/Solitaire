be3e93e8032f9822d0fed256588613b46b4c9136
Yashik <30672986+dyashik@users.noreply.github.com>
2022-01-16 11:57:09 -0800

    Update CardTester.java

--- CardTester.java
+++ CardTester.java
@@ -1,16 +1,3 @@
-/**
- * Author: Yashik Dhanaraj
- * Period: 5
- * Date: 12/2
- * Time Taken: 2hr
- * 
- * Summary: I did much better than I thought. I had an error but it
- * didn't turn out to be my understanding but it was some glitch with the 
- * compiler stuff. But Mr. McLeod was able to fix it. Something really neat
- * that i did was I had a printList method which printed the list with the 
- * suits next to the symbol to make it a more user friendly.
- * 
- **/
 public class CardTester {
     public static void main(String[] args) {
         Card card = new Card(7, "Hearts");
